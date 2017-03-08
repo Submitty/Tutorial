575f787fb2e31ab7d0782b3a4ffb0ea9cdd7524b
Samuel Breese <samuel@chame.co>
2017-03-08 14:24:36 -0500

    Delete the old ones

--- examples/11/submissions/buggy.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <unistd.h>
-
-int main()
-{
-	sleep(1000);
-	std::cout << "Goodbye, world!" << std::endl;
-}
--- examples/11/submissions/solution.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <unistd.h>
-
-int main()
-{
-	sleep(50);
-	std::cout << "Hello, world!" << std::endl;
-}
