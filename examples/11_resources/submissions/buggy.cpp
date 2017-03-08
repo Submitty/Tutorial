f88bdc652a39e0ec4ee2b261393c7441dd4c2515
Samuel Breese <samuel@chame.co>
2017-03-08 14:23:44 -0500

    Rename

---
+++ examples/11_resources/submissions/buggy.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <unistd.h>
+
+int main()
+{
+	sleep(1000);
+	std::cout << "Goodbye, world!" << std::endl;
+}
---
+++ examples/11_resources/submissions/solution.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <unistd.h>
+
+int main()
+{
+	sleep(50);
+	std::cout << "Hello, world!" << std::endl;
+}
