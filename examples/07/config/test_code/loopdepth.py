058d3b38bde3c24970ccbb10da79ff7075f90015
Samuel Breese <samuel@chame.co>
2017-03-08 13:56:21 -0500

    Added last four

---
+++ examples/07/config/test_code/loopdepth.py
@@ -0,0 +1,4 @@
+import sys
+import lang.parser
+paths = lang.parser.leaf_paths(lang.parser.python(open(sys.argv[1]).read()))
+print(max([len([x for x in path if x.name in ["for", "while"]]) for path in paths]))
