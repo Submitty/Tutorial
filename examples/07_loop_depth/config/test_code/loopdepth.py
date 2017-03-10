f8e98be1db6eb6afc8c32dda035e19fc9c2ffcd5
Samuel Breese <samuel@chame.co>
2017-03-10 01:41:39 -0500

    Fix path

--- examples/07_loop_depth/config/test_code/loopdepth.py
+++ examples/07_loop_depth/config/test_code/loopdepth.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("/usr/local/SubmittyAnalysisTools/")
+sys.path.append("/usr/local/submitty/SubmittyAnalysisTools/")
 import lang.parser
 paths = lang.parser.leaf_paths(lang.parser.python(open(sys.argv[1]).read()))
 print(max([len([x for x in path if x.name in ["for", "while"]]) for path in paths]))
