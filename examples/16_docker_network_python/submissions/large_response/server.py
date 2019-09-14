154434442abecb8e31b5fae9582760700848a227
emaicus <emaicus@outlook.com>
2019-09-14 14:31:21 -0400

    Changed Tutorial 16 from ping pong to zig zag (#25)

--- examples/16_docker_network_python/submissions/large_response/server.py
+++ examples/16_docker_network_python/submissions/large_response/server.py
@@ -3,147 +3,79 @@ import socket
 import os
 import sys
 import time
+import json
 import traceback
+import threading
 
 MY_NAME = ""
-KNOWN_HOSTS_TCP = 'knownhosts_tcp.txt'
-
-# This tutorial is kept simple intentionally rather than using data structures or
-# dictionaries to store these values.
-client_name = ""
-incoming_port = 0
-outgoing_port = 1
-outgoing_socket = None
-incoming_socket = None
-open_connection = None
-
-def init():
-  read_known_hosts_tcp()
-  initialize_incoming_connections()
-
+KNOWN_HOSTS_JSON = 'knownhosts.json'
+ADDRESS_BOOK = dict()
+IP_LOOKUP = dict()
+RUNNING = False
+INCOMING_PORT = -1
 
 #knownhosts_tcp.csv and knownhosts_udp.csv are of the form
 #sender,recipient,port_number
 # such that sender sends all communications to recipient via port_number. 
-def read_known_hosts_tcp():
-  global client_name
-  global incoming_port
-  global outgoing_port
-  with open(KNOWN_HOSTS_TCP) as infile:
-    content = infile.readlines()
+def read_known_hosts_json():
+  global INCOMING_PORT, ADDRESS_BOOK, IP_LOOKUP
+  with open(KNOWN_HOSTS_JSON) as infile:
+    content = json.load(infile)
   
-  for line in content:
-    sender, recv, port = line.split()
-    if sender == MY_NAME:
-      outgoing_port =port
-      client_name = recv
-    elif recv == MY_NAME:
-      incoming_port = port
-      client_name = sender
+  for host, info in content['hosts'].items():
+    port = info['tcp_start_port']
+    IP_LOOKUP[info['ip_address']] = host
+    
+    if host == MY_NAME:
+      INCOMING_PORT = int(port)
     else:
+      ADDRESS_BOOK[host] = int(port)
+
+  if INCOMING_PORT == -1:
+    raise SystemExit(f'ERROR: No entry for this host in {KNOWN_HOSTS_JSON}')
+
+
+def listen_for_incoming_messages():
+  global RUNNING, IP_LOOKUP, ADDRESS_BOOK
+
+  serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  serversocket.bind(('', INCOMING_PORT))
+  serversocket.listen(5)
+
+  while RUNNING:
+    try:
+      (clientsocket, address) = serversocket.accept()
+    except socket.timeout as e:
+      print("ERROR!")
       continue
+    message = clientsocket.recv(1024).decode('utf-8')
 
-def initialize_incoming_connections():
-    global incoming_socket
+    parts = message.split(':')
 
-    incoming_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('', int(incoming_port))
-    incoming_socket.bind(server_address)
-    incoming_socket.listen(5)
-    incoming_socket.setblocking(False)
-    print('Listening on port {0} for incoming tcp connections'.format(incoming_port))
+    while parts[0] == MY_NAME:
+      print(f'Forwarding {parts} to myself.')
+      parts.pop(0)
 
-def init_outgoing_connection():
-  global outgoing_socket
-  if outgoing_socket != None:
-    return True
+    if len(parts) == 0 or parts[0] == 'FINISHED!':
+      print('Finished!')
+    else:
+      next_message = 'LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGELONG MESSAGE LONG MESSAGE LONG MESSAGE LONG MESSAGE'
+      next_host = parts[0]
+      print(f'Forwarding {next_message} to {parts[0]}')
+      
+      outgoing_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      outgoing_socket.connect((next_host, ADDRESS_BOOK[next_host]))
+      outgoing_socket.sendall(next_message.encode('utf-8'))
 
-  server_address = (client_name, int(outgoing_port))
-  outgoing_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-  try:
-    outgoing_socket.connect(server_address)
-    return True
-  except Exception as e:
-    print('Unable to connect on {0}'.format(server_address))
-    return False
-
-def send_out_message(msg):
-  global outgoing_socket
-  if outgoing_socket == None:
-    print("ERROR! Could not establish outgoing connection to {0}".format(client_name))
-    sys.exit(1)
-  outgoing_socket.sendall(msg.encode('utf-8'))
-
-def cleanup():
-  global outgoing_socket
-  global incoming_socket
-  global open_connection
-
-  if outgoing_socket != None:
-    outgoing_socket.close()
-  if incoming_socket != None:
-    incoming_socket.close()
-  if open_connection != None:
-    open_connection.close()
-
-def check_for_request():
-  global open_connection
-  global incoming_socket
-
-  try:
-    if open_connection == None:
-      open_connection, client_address = incoming_socket.accept()
-      open_connection.setblocking(False)
-
-    message = open_connection.recv(1024)
-    return message.decode('utf-8')
-  except BlockingIOError as e:
-    pass
-  except Exception as e:
-    print(traceback.format_exc())
-    sys.stdout.flush()
-  return None
-
-def sendPong():
-  if init_outgoing_connection():
-    response = "pong" * 1000000
-    send_out_message(response) 
-  else:
-    print("ERROR: could not send pong.")
-
-def send_err(old_message):
-  if init_outgoing_connection():
-    print("Sending 'Invalid Message {0}' to {1}".format(old_message, client_name))
-    send_out_message("Invalid Message '{0}'".format(old_message)) 
-  else:
-    print("ERROR: could not send error message.")
-
-def run():
-  running = True
-  while running:
-    message = check_for_request()
-    if message == '':
-      print('{0} disconnected.'.format(client_name))
-      running = False
-      continue
-    if message != None:
-      print("Recieved '{0}' from {1}".format(message, client_name))
-      if message == "ping":
-        sendPong()
-      else:
-        send_err(message)
-    sys.stdout.flush()
-    time.sleep(.1)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print("ERROR: Please pass the following arguments:\n\t1: The name of this host in the knownhosts.csv file.")
-    sys.stdout.flush()
-    sys.exit(1)
-  MY_NAME = sys.argv[1]
-  
-  init()
-  run()
-  cleanup()
+
+  MY_NAME = socket.gethostname()
+  RUNNING = True
+
+  read_known_hosts_json()
+  try:
+    listen_for_incoming_messages()
+  except KeyboardInterrupt:
+    sys.exit(0)
--- examples/16_docker_network_python/submissions/udp_correct/server.py
+++ examples/16_docker_network_python/submissions/udp_correct/server.py
@@ -1,220 +1,78 @@
 #!/usr/bin/env python3
 import socket
 import os
-import csv
 import sys
 import time
+import json
 import traceback
-
-KNOWN_HOSTS_TCP = "knownhosts_tcp.txt"
-KNOWN_HOSTS_UDP = "knownhosts_udp.txt"
+import threading
 
 MY_NAME = ""
-USE_UDP = False
-
-client_name = ""
-
-incoming_tcp_port = 0
-outgoing_tcp_port = 1
-incoming_udp_port = 2
-outgoing_udp_port = 3
-
-outgoing_tcp_socket = None
-incoming_tcp_socket = None
-open_tcp_connection = None
-
-outgoing_udp_socket = None
-incoming_udp_socket = None
-
-def init():
-  read_known_hosts_csv()
-  initialize_incoming_connections()
-
+KNOWN_HOSTS_JSON = 'knownhosts.json'
+ADDRESS_BOOK = dict()
+IP_LOOKUP = dict()
+RUNNING = False
+INCOMING_PORT = -1
 
 #knownhosts_tcp.csv and knownhosts_udp.csv are of the form
 #sender,recipient,port_number
 # such that sender sends all communications to recipient via port_number. 
-def read_known_hosts_csv():
-  global client_name
-  global incoming_tcp_port, outgoing_tcp_port
-  global incoming_udp_port, outgoing_udp_port
-  with open(KNOWN_HOSTS_TCP) as infile:
-    content = infile.readlines()
+def read_known_hosts_json():
+  global INCOMING_PORT, ADDRESS_BOOK, IP_LOOKUP
+  with open(KNOWN_HOSTS_JSON) as infile:
+    content = json.load(infile)
   
-  for line in content:
-    sender, recv, port = line.split()
-    if sender == MY_NAME:
-      outgoing_tcp_port =port
-      client_name = recv
-    elif recv == MY_NAME:
-      incoming_tcp_port = port
-      client_name = sender
+  for host, info in content['hosts'].items():
+    port = info['udp_start_port']
+    IP_LOOKUP[info['ip_address']] = host
+    
+    if host == MY_NAME:
+      INCOMING_PORT = int(port)
     else:
-      continue
+      ADDRESS_BOOK[host] = int(port)
 
-  if USE_UDP:
-    with open(KNOWN_HOSTS_UDP) as infile:
-      content = infile.readlines()
-    
-    for line in content:
-      sender, recv, port = line.split()
-      if sender == MY_NAME:
-        outgoing_udp_port = port
-        client_name = recv
-      elif recv == MY_NAME:
-        incoming_udp_port = port
-        client_name = sender
-      else:
-        continue
+  if INCOMING_PORT == -1:
+    raise SystemExit(f'ERROR: No entry for this host in {KNOWN_HOSTS_JSON}')
 
-def initialize_incoming_connections():
-  global incoming_tcp_socket, incoming_udp_socket
-  incoming_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  server_address = ('', int(incoming_tcp_port))
-  incoming_tcp_socket.bind(server_address)
-  incoming_tcp_socket.listen(5)
-  incoming_tcp_socket.setblocking(False)
-  print('Listening on port {0} for incoming tcp connections'.format(incoming_tcp_port))
 
-  if USE_UDP:
-    incoming_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    incoming_udp_socket.setblocking(False)
-    server_address = ('', int(incoming_udp_port))
-    incoming_udp_socket.bind(server_address)
-    print('Listening on port {0} for incoming udp connections'.format(incoming_udp_port))
+def listen_for_incoming_messages():
+  global RUNNING, IP_LOOKUP, ADDRESS_BOOK
 
-def init_outgoing_connection(connection_type):
-  global outgoing_tcp_socket, outgoing_tcp_port
-  global outgoing_udp_socket, outgoing_udp_port
+  udp_socket =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  udp_socket.bind(('', INCOMING_PORT))
+  udp_socket.settimeout(1)
 
-  if connection_type == 'tcp':
-    if outgoing_tcp_socket != None:
-      return True
-    server_address = (client_name, int(outgoing_tcp_port))
-    outgoing_tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  while RUNNING:
     try:
-      outgoing_tcp_socket.connect(server_address)
-      return True
-    except Exception as e:
-      print('Unable to connect on {0} (tcp)'.format(server_address))
-      traceback.print_exc()
-      return False
-  elif connection_type == 'udp':
-    if outgoing_udp_socket != None:
-      return True
-    server_address = (client_name, int(outgoing_udp_port))
-    outgoing_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return True
-  return False
-    
-
-
-def send_out_message(msg, tcp=True):
-  global outgoing_tcp_socket, outgoing_udp_socket
-  if tcp and outgoing_tcp_socket == None:
-    print("ERROR! Could not establish outgoing tcp connection to {0}".format(client_name))
-    sys.exit(1)
-  elif tcp == False and outgoing_udp_socket == None:
-    print("ERROR! Could not establish outgoing udp connection to {0}".format(client_name))
-    sys.exit(1)
-  
-  if tcp:
-    print("Sending '{0}' to {1}".format(msg, client_name, outgoing_tcp_port))
-    sys.stdout.flush()
-    outgoing_tcp_socket.sendall(msg.encode('utf-8'))
-  else:
-    print("Sending '{0}' to {1}".format(msg, client_name, outgoing_udp_port))
-    sys.stdout.flush()
-    destination_address = (client_name, int(outgoing_udp_port))
-    outgoing_udp_socket.sendto(msg.encode('utf-8'),destination_address)
-
-
-def cleanup():
-  global incoming_tcp_socket, outgoing_tcp_socket, open_tcp_connection, outgoing_udp_socket, incoming_udp_socket
-  if incoming_tcp_socket != None:
-    incoming_tcp_socket.close()
-  if outgoing_tcp_socket != None:
-    outgoing_tcp_socket.close()
-  if open_tcp_connection != None:
-    open_tcp_connection.close()
-  if outgoing_udp_socket != None:
-    outgoing_udp_socket.close()
-  if incoming_udp_socket != None:
-    incoming_udp_socket.close()
-
-def check_for_request():
-  global open_tcp_connection, incoming_tcp_socket, incoming_udp_socket
-  try:
-    if open_tcp_connection == None:
-      open_tcp_connection, client_address = incoming_tcp_socket.accept()
-      open_tcp_connection.setblocking(False)
-
-    message = open_tcp_connection.recv(1024)
-    return message.decode('utf-8'), True
-  except BlockingIOError as e:
-    #print("Exception encountered. Shouldn't be a big deal.")
-    pass
-  except Exception as e:
-    print(traceback.format_exc())
-
-  if USE_UDP:
-    try:
-      message = incoming_udp_socket.recv(1024)
-      return message.decode('utf-8'), False
-    except socket.error:
-      pass
-    except Exception as e:
-      print(traceback.format_exc())
-
-  return None, None
-
-def sendPong(tcp=True):
-  connection_type = 'tcp' if tcp else 'udp'
-  if init_outgoing_connection(connection_type):
-    send_out_message("pong",tcp=tcp) 
-  else:
-    print("ERROR: could not send pong.")
-
-def send_err(old_message,tcp=True):
-  connection_type = 'tcp' if tcp else 'udp'
-  if init_outgoing_connection(connection_type):
-    print("Sending 'Invalid Message {0}' to {1}".format(old_message, client_name))
-    send_out_message("Invalid Message '{0}'".format(old_message),tcp=tcp) 
-  else:
-    print("ERROR: could not send error message.")
-
-def run():
-  running = True
-  while running:
-    sys.stdout.flush()
-    message, tcp_message = check_for_request()
-    if message == '':
-      print('{0} disconnected.'.format(client_name))
-      sys.stdout.flush()
-      running = False
+      msg, addr = udp_socket.recvfrom(1024)
+    except socket.timeout as e:
       continue
-    if message != None:
-      print("Recieved '{0}' from {1}".format(message, client_name))
-      sys.stdout.flush()
-      if message == "ping":
-        sendPong(tcp=tcp_message)
-      else:
-        send_err(message,tcp=tcp_message)
-    sys.stdout.flush()
-    time.sleep(.1)
+    message = msg.decode('utf-8')
+
+    parts = message.split(':')
+
+    while parts[0] == MY_NAME:
+      print(f'Forwarding {parts} to myself.')
+      parts.pop(0)
+
+    if len(parts) == 0 or parts[0] == 'FINISHED!':
+      print('Finished!')
+    else:
+      next_message = ':'.join(parts[1:])
+      next_host = parts[0]
+      print(f'Forwarding {next_message} to {parts[0]}')
+      
+      udp_socket.sendto(next_message.encode('utf-8'), (parts[0], ADDRESS_BOOK[next_host]))
+
 
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print("ERROR: Please pass the following arguments:\n\t1: The name of this host in the knownhosts.csv file.")
-    sys.stdout.flush()
-    sys.exit(1)
-  MY_NAME = sys.argv[1]
 
-  if len(sys.argv) > 2:
-    if sys.argv[2].strip() == 'udp_enabled':
-      USE_UDP=True
-  init()
+  MY_NAME = socket.gethostname()
+  RUNNING = True
 
-  run()
-  cleanup()
+  read_known_hosts_json()
+  try:
+    listen_for_incoming_messages()
+  except KeyboardInterrupt:
+    sys.exit(0)
