2119b10d6bd5cce8e6eea96ca9067d3b293ed5cd
William Allen <16820599+williamjallen@users.noreply.github.com>
2022-06-10 01:32:20 -0400

    [Testing:Developer] Update example submissions to Python3 (#36)

--- examples/03_multipart/submissions/part1_syntax_error1.py
+++ examples/03_multipart/submissions/part1_syntax_error1.py
@@ -1,2 +1,2 @@
-print "The square root of 16:", math.sqrt(16)
-print "The square root of 9:", math.sqrt(9)
+print("The square root of 16:", math.sqrt(16))
+print("The square root of 9:", math.sqrt(9))
